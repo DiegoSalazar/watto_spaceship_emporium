b1adc963a4bebf8e69c32d1b1b9f92975b90299c
Diego Salazar <diego@greyrobot.com>
2017-04-20 01:24:22 -0400

    refactor

--- app/presenters/product_presenter.rb
+++ app/presenters/product_presenter.rb
@@ -1,7 +1,7 @@
 class ProductPresenter < Presenter
   def tech_specs_as_dictionary_list
     list = '<dl class="dl-horizontal">'.html_safe
-    object.tech_specs.each do |term, description|
+    tech_specs.each do |term, description|
       list << view_context.content_tag(:dt, term.titleize)
       list << view_context.content_tag(:dd, description)
     end
